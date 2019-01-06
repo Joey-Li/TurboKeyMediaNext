17ee786de052280fed65bc68d764bbf397d71816
Joey <32761572+Joey-Li@users.noreply.github.com>
2019-01-06 16:35:29 +0800

    Updated Version 1.0.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # TurboKeyMediaNext
-Create TurboKey function, it will pass Keyevent_Media_Next to system when clicking TurboKey. It's only for Honor Note10 Device.
+Create TurboKey alternative function.
+TurboKey will pass Keyevent_Media_Next to system when clicking TurboKey. It's only for Honor Note10 Device.
 
 
 TurboKey : KeyEvent is 702
--- app/src/main/java/joeylang/com/turbokeymedianext.java
+++ app/src/main/java/joeylang/com/turbokeymedianext.java
@@ -1,46 +1,64 @@
 package joeylang.com.turbokeymedianext;
 
 import android.accessibilityservice.AccessibilityService;
-import android.accessibilityservice.AccessibilityServiceInfo;
-import android.annotation.SuppressLint;
-import android.app.Instrumentation;
-import android.content.Intent;
-import android.util.Log;
+//import android.content.Intent;
+import android.media.AudioManager;
+import android.os.PowerManager;
+import android.os.SystemClock;
+//import android.util.Log;
 import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityNodeInfo;
-
-import java.io.IOException;
-
+//import android.view.accessibility.AccessibilityNodeInfo;
 
 public class turbokeymedianext extends AccessibilityService {
-    private static final String TAG = "TurboKey2MediaNext";
-    String[] PACKAGES = { "com.android.settings" };
+    //private static final String TAG = "TurboKey2MediaNext";
+    private AudioManager mAudioManager;
+    private PowerManager.WakeLock mWakeLock;
 
     @Override
     protected boolean onKeyEvent(KeyEvent event) {
-        int flag=0;
+
         int key = event.getKeyCode();
-        Log.i(TAG, "onKeyEvent "+ key);
-        switch(key){
-            case 702:  //Note 10, Turbo Key Code
-                Log.i(TAG, "onKeyEvent "+ key +" Start");
-                //Send broadcast intent to main activity.
-                //On the main activity you can take any desired action.
+        int key_action_code = event.getAction();
+        long eventtime = SystemClock.uptimeMillis();
+        //Log.i(TAG, "onKeyEvent key: "+ key);
+        //Log.i(TAG, "onKeyEvent event.getAction(): "+ event.getAction());
 
-                Intent media_intent = new Intent("com.exmaple.broadcaster.KEYDOWN");
-                media_intent.putExtra("dtime", System.currentTimeMillis());
-                if(flag==0){
-                    sendBroadcast(media_intent);
-                    flag+=1;
-                }else if (flag==1) {
-                    flag=0;
-                }
-                Log.i(TAG, "onKeyEvent "+ key +" End");
+        if (key_action_code==KeyEvent.ACTION_UP)
+        {
 
-                break;
-            default:
-                break;
+            switch (key) {
+                case 702:  //Note 10, Turbo Key Code
+                    //Log.i(TAG, "ACTION_UP onKeyEvent " + key + " Start");
+                    if (mAudioManager == null) mAudioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+                    //Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+                    KeyEvent upEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_NEXT, 0);
+                    //upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
+                    //sendOrderedBroadcast(upIntent, null);
+                    mAudioManager.dispatchMediaKeyEvent(upEvent);
+                    //Log.i(TAG, "ACTION_UP onKeyEvent " + key + " End");
+
+                    break;
+                default:
+                    break;
+            }
+        }else if (key_action_code==KeyEvent.ACTION_DOWN)
+        {
+            switch (key) {
+                case 702:  //Note 10, Turbo Key Code
+                    //Log.i(TAG, " ACTION_DOWN onKeyEvent " + key + " Start");
+                    if (mAudioManager == null) mAudioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+                    //Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+                    KeyEvent downEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_NEXT, 0);
+                    //downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
+                    //sendOrderedBroadcast(downIntent, null);
+                    mAudioManager.dispatchMediaKeyEvent(downEvent);
+                    //Log.i(TAG, "ACTION_DOWN onKeyEvent " + key + " End");
+
+                    break;
+                default:
+                    break;
+            }
         }
         return super.onKeyEvent(event);
     }
@@ -48,79 +66,88 @@ public class turbokeymedianext extends AccessibilityService {
     @Override
     protected void onServiceConnected() {
 
-        Log.i(TAG, "config success!");
+        //Log.i(TAG, "config success!");
+
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         // TODO Auto-generated method stub
-        int eventType = event.getEventType();
-        Log.i(TAG, "onAccessibilityEvent");
-        String eventText = "";
-        switch (eventType) {
-            case AccessibilityEvent.TYPE_VIEW_CLICKED:
-                Log.i(TAG, "==============Start====================");
-                eventText = "TYPE_VIEW_CLICKED";
-                AccessibilityNodeInfo noteInfo = event.getSource();
-                Log.i(TAG, noteInfo.toString());
-                Log.i(TAG, "=============END=====================");
-                break;
-            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
-                eventText = "TYPE_VIEW_FOCUSED";
-                break;
-            case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
-                eventText = "TYPE_VIEW_LONG_CLICKED";
-                break;
-            case AccessibilityEvent.TYPE_VIEW_SELECTED:
-                eventText = "TYPE_VIEW_SELECTED";
-                break;
-            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
-                eventText = "TYPE_VIEW_TEXT_CHANGED";
-                break;
-            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
-                eventText = "TYPE_WINDOW_STATE_CHANGED";
-                break;
-            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
-                eventText = "TYPE_NOTIFICATION_STATE_CHANGED";
-                break;
-            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END:
-                eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_END";
-                break;
-            case AccessibilityEvent.TYPE_ANNOUNCEMENT:
-                eventText = "TYPE_ANNOUNCEMENT";
-                break;
-            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:
-                eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_START";
-                break;
-            case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:
-                eventText = "TYPE_VIEW_HOVER_ENTER";
-                break;
-            case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT:
-                eventText = "TYPE_VIEW_HOVER_EXIT";
-                break;
-            case AccessibilityEvent.TYPE_VIEW_SCROLLED:
-                eventText = "TYPE_VIEW_SCROLLED";
-                break;
-            case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED:
-                eventText = "TYPE_VIEW_TEXT_SELECTION_CHANGED";
-                break;
-            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
-                eventText = "TYPE_WINDOW_CONTENT_CHANGED";
-                break;
-        }
+//        int eventType = event.getEventType();
+//        Log.i(TAG, "onAccessibilityEvent");
+//        String eventText = "";
+//        switch (eventType) {
+//            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+//                Log.i(TAG, "==============Start====================");
+//                eventText = "TYPE_VIEW_CLICKED";
+//                Log.i(TAG, "=============END=====================");
+//                break;
+//            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+//                eventText = "TYPE_VIEW_FOCUSED";
+//                break;
+//            case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
+//                eventText = "TYPE_VIEW_LONG_CLICKED";
+//                break;
+//            case AccessibilityEvent.TYPE_VIEW_SELECTED:
+//                eventText = "TYPE_VIEW_SELECTED";
+//                break;
+//            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
+//                eventText = "TYPE_VIEW_TEXT_CHANGED";
+//                break;
+//            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+//                eventText = "TYPE_WINDOW_STATE_CHANGED";
+//                break;
+//            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+//                eventText = "TYPE_NOTIFICATION_STATE_CHANGED";
+//                break;
+//            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END:
+//                eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_END";
+//                break;
+//            case AccessibilityEvent.TYPE_ANNOUNCEMENT:
+//                eventText = "TYPE_ANNOUNCEMENT";
+//                break;
+//            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:
+//                eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_START";
+//                break;
+//            case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:
+//                eventText = "TYPE_VIEW_HOVER_ENTER";
+//                break;
+//            case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT:
+//                eventText = "TYPE_VIEW_HOVER_EXIT";
+//                break;
+//            case AccessibilityEvent.TYPE_VIEW_SCROLLED:
+//                eventText = "TYPE_VIEW_SCROLLED";
+//                break;
+//            case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED:
+//                eventText = "TYPE_VIEW_TEXT_SELECTION_CHANGED";
+//                break;
+//            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+//                eventText = "TYPE_WINDOW_CONTENT_CHANGED";
+//                break;
+//        }
+//
+//        Log.i(TAG, "onAccessibilityEvent " + eventText);
     }
 
     @Override
     public void onInterrupt() {
         // TODO Auto-generated method stub
+        //((PowerManager.WakeLock) mWakeLock).release();
+        //Log.i(TAG, "::onInterrupt");
 
     }
 
     @Override
     public void onCreate() {
-        Log.i(TAG, "::onCreate");
+
+        //Log.i(TAG, "::onCreate");
+        //PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        //mWakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"MyWakelockTag");
+        //((PowerManager.WakeLock) mWakeLock).acquire();
+
         super.onCreate();
     }
+
 
 
 }
